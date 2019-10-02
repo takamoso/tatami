f272b15153ddb613b31d5d03484f0e2b26479cdf
takamoso <htmlcssjslove@gmail.com>
2019-10-02 21:36:52 +0900

    Support file import to _base64-encode()

--- src/utility/_base64-encode.js
+++ src/utility/_base64-encode.js
@@ -1,13 +1,35 @@
+const fs = require('fs')
+const path = require('path')
 const sass = require('node-sass')
+const root = require('app-root-path')
 
 module.exports = {
-  '_base64_encode($string)': string => {
-    if (!string instanceof sass.types.String) {
+  '_base64_encode($data, $extension: false)': (data, extension) => {
+    if (!data instanceof sass.types.String) {
       return
     }
 
-    const buffer = Buffer.from(string.getValue())
+    data = data.getValue()
+    extension = extension.getValue()
 
-    return new sass.types.String(buffer.toString('base64'))
+    const extname = path.extname(data).toLowerCase().replace('.', '')
+    const list = new sass.types.List(2)
+
+    if (extname === '') {
+      const base64 = new sass.types.String(Buffer.from(data).toString('base64'))
+
+      list.setValue(0, base64)
+      list.setValue(1, new sass.types.String(extname))
+
+      return extension ? list : base64
+    }
+
+    const file = fs.readFileSync(root.resolve(data))
+    const base64 = new sass.types.String(Buffer.from(file).toString('base64'))
+
+    list.setValue(0, base64)
+    list.setValue(1, new sass.types.String(extname))
+    
+    return extension ? list : base64
   }
 }
