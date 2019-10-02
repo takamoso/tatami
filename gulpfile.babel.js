a06405848c4b0ec10b49ba98c7d6329f74a48a1a
takamoso <htmlcssjslove@gmail.com>
2019-10-02 16:01:43 +0900

    Fix development environment

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,11 @@
 import gulp from 'gulp'
 import sass from 'gulp-sass'
 
-export const test = () => {
+export const test = async () => {
   return gulp.src('./test/style.scss')
     .pipe(sass({
       outputStyle: 'expanded',
+      functions: await import('./src/tatami'),
     }))
     .pipe(gulp.dest('./test'))
 }
