9e4bcf1483ad97ed347d7a83867c145ceeee88f1
takamoso <htmlcssjslove@gmail.com>
2019-10-04 14:52:11 +0900

    Add _exp()

---
+++ src/utility/_exp.js
@@ -0,0 +1,13 @@
+const sass = require('node-sass')
+
+module.exports = {
+  '_exp($x)': x => {
+    if (!(x instanceof sass.types.Number)) {
+      throw "$x: Expected a number."
+    } else if (x.getUnit()) {
+      throw '$x: Expected a unitless number.'
+    }
+
+    return new sass.types.Number(Math.exp(x.getValue()))
+  }
+}
