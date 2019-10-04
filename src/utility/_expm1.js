073ba5bab4c90fd0bf296e0800abc7fd7e779ead
takamoso <htmlcssjslove@gmail.com>
2019-10-04 14:52:22 +0900

    Add _expm1()

---
+++ src/utility/_expm1.js
@@ -0,0 +1,13 @@
+const sass = require('node-sass')
+
+module.exports = {
+  '_expml($x)': x => {
+    if (!(x instanceof sass.types.Number)) {
+      throw "$x: Expected a number."
+    } else if (x.getUnit()) {
+      throw '$x: Expected a unitless number.'
+    }
+
+    return new sass.types.Number(Math.expm1(x.getValue()))
+  }
+}
