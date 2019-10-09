11146bdc9da02fb4b3a2f8f050a6e1d2931547f8
takamoso <htmlcssjslove@gmail.com>
2019-10-09 21:15:13 +0900

    Fix development environment

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,17 @@
 import gulp from 'gulp'
+import plumber from 'gulp-plumber'
+import touch from 'gulp-touch-cmd'
 import sass from 'gulp-sass'
 
 export const test = async () => {
   return gulp.src('./test/style.scss')
+    .pipe(plumber())
     .pipe(sass({
       outputStyle: 'expanded',
       functions: await import('./src/tatami'),
     }))
     .pipe(gulp.dest('./test'))
+    .pipe(touch())
 }
 
 export const watch = () => {
