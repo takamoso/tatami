a61dcc879879bb83a4bb5f2754c2279f013a4715
takamoso <htmlcssjslove@gmail.com>
2019-11-03 22:24:48 +0900

    Fix minify html timing in documentation

--- doc/themes/theme/scripts/minify-html.js
+++ doc/themes/theme/scripts/minify-html.js
@@ -2,12 +2,15 @@
  * HTMLの圧縮
  */
 
+const path = require('path')
 const htmlMinifier = require('html-minifier')
 
-hexo.extend.filter.register('after_render:html', str => {
-  return htmlMinifier.minify(str, {
-    collapseBooleanAttributes: true,
-    collapseWhitespace: true,
-    removeAttributeQuotes: true,
-  })
+hexo.extend.filter.register('after_render:html', (str, data) => {
+  if (path.extname(data.path) === '.ejs') {
+    return htmlMinifier.minify(str, {
+      collapseBooleanAttributes: true,
+      collapseWhitespace: true,
+      removeAttributeQuotes: true,
+    })
+  }
 })
